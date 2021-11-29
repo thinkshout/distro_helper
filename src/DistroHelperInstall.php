5d6487df61db8b867d035995a4f21bd870b5996a
Maria Fisher <maria.fisher@thinkshout.com>
2021-11-28 16:44:33 -0800

    PHPCS.

--- src/DistroHelperInstall.php
+++ src/DistroHelperInstall.php
@@ -3,15 +3,10 @@
 namespace Drupal\distro_helper;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Config\ConfigManagerInterface;
 use Drupal\Core\Config\StorageInterface;
-use Drupal\Core\Config\CachedStorage;
-use Drupal\Component\Serialization\Yaml;
-use Drupal\Component\Utility\Crypt;
-use Drupal\Core\Site\Settings;
 
 /**
- * Provides a service to help with configuration management in distros on install.
+ * Provides a service to help with config management in distros on install.
  */
 class DistroHelperInstall {
 
@@ -38,25 +33,14 @@ class DistroHelperInstall {
   }
 
   /**
-   * Helper function to update the uuids from the just-installed site to match the config folder.
+   * Update the uuids from the just-installed site to match the config folder.
    *
-   * @param string $configName
+   * @param string $configs
    *   The name of the config (its file name with the '.yml' part).
-   * @param string $module
-   *   Module machine name that has the config files.
-   * @param string $directory
-   *   Usually "install" but sometimes "optional".
-   * @param bool $update
-   *   Whether or not to update the config if it already exists.
-   *
-   * @return array[]
-   *   Two arrays of 'updated' and 'created' configurations.
-   *
-   * @throws \Drupal\Core\Entity\EntityStorageException
    */
-  public function syncUUIDs($configs) {
+  public function syncUuids($configs) {
     // Foreach config in the system.
-    foreach($configs as $configName) {
+    foreach ($configs as $configName) {
       // If new config exists in sync, match up the uuids.
       $sync_config = $this->configStorageSync->read($configName);
       $entity = $this->configFactory->getEditable($configName);
@@ -70,4 +54,5 @@ class DistroHelperInstall {
       $entity->save();
     }
   }
+
 }
