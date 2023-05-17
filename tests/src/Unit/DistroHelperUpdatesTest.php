a0ce18cfa23dde663fdcfc8b03d87e00b333ce21
Maria Fisher <maria.fisher@thinkshout.com>
2023-05-17 13:01:37 -0700

    Seeing if this allows us to use the logger.

--- tests/src/Unit/DistroHelperUpdatesTest.php
+++ tests/src/Unit/DistroHelperUpdatesTest.php
@@ -7,7 +7,7 @@ use Drupal\Core\Config\ConfigManagerInterface;
 use Drupal\Core\Config\StorageInterface;
 use Drupal\Tests\UnitTestCase;
 use Drupal\distro_helper\DistroHelperUpdates;
-use Drupal\Core\Logger\LoggerChannelFactory;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 
 /**
  * Simple test to ensure that asserts pass.
@@ -74,7 +74,7 @@ class DistroHelperUpdatesTest extends UnitTestCase {
     $config_manager = $this->prophesize(ConfigManagerInterface::class);
     $config_storage_sync = $this->prophesize(StorageInterface::class);
     $config_storage = $this->prophesize(CachedStorage::class);
-    $logger = $this->prophesize(LoggerChannelFactory::class);
+    $logger = $this->prophesize(LoggerChannelFactoryInterface::class);
     $this->distroHelperUpdates = new DistroHelperUpdates($config_manager->reveal(), $config_storage_sync->reveal(), $config_storage->reveal(), $logger->reveal());
   }
 
