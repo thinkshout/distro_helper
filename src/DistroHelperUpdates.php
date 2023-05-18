25d84f019bdb9adc2134b1f34d4b6455a15bd021
Maria Fisher <mariacha@gmail.com>
2023-05-18 12:48:20 -0700

    Better warning
    Co-authored-by: Gabe Carleton-Barnes <gabe@thinkshout.com>

--- src/DistroHelperUpdates.php
+++ src/DistroHelperUpdates.php
@@ -196,7 +196,7 @@ class DistroHelperUpdates {
     if ($active_config->isNew()) {
       // Can't update nonexistent config.
       $this->logger->error(
-        'No active config found for @configName. Use installConfig to import config that does not already exist in your database.',
+        'No active config found for @configName in updateConfig(). Use installConfig() to import config that does not already exist in your database.',
         ['@config' => $configName]);
       return FALSE;
     }
